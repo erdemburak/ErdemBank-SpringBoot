9022d254c13ed572976272a40db2e6250df032b2
erdemburak <burakerdem6@hotmail.com>
2024-02-06 19:39:05 +0300

    City(Enum) modeli olusturuldu

---
+++ src/main/java/com/erdemburak/dto/CityDto.java
@@ -0,0 +1,5 @@
+package com.erdemburak.dto;
+
+public enum CityDto {
+    ISTANBUL,ANKARA,IZMIR,MANISA,MARAS,KOCAELI
+}
---
+++ src/main/java/com/erdemburak/model/City.java
@@ -0,0 +1,5 @@
+package com.erdemburak.model;
+
+public enum City {
+    ISTANBUL,ANKARA,IZMIR,MANISA,MARAS,KOCAELI
+}
