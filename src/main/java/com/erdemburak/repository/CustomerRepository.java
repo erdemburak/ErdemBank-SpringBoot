6d752439bc4b9e10799c72d16c330d333f9e3d12
erdemburak <burakerdem6@hotmail.com>
2024-02-06 19:39:42 +0300

    CustomerRepository olusturuldu

---
+++ src/main/java/com/erdemburak/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.erdemburak.repository;
+
+import com.erdemburak.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
+}
