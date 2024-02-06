8af25b377d391058fa2a5fcd38880e9aa9a913bf
erdemburak <burakerdem6@hotmail.com>
2024-02-06 19:33:42 +0300

    ErdemBank uygulamasi olusturuldu

---
+++ src/main/java/com/erdemburak/ErdembankApplication.java
@@ -0,0 +1,13 @@
+package com.erdemburak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErdembankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErdembankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erdemburak/ErdembankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdemburak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdembankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
