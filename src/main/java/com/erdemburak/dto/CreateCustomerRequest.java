45bcd4807c77cac6bfaa457f0aaa23fabf8e4d7e
erdemburak <burakerdem6@hotmail.com>
2024-02-07 19:45:01 +0300

    Builder eklendi

--- src/main/java/com/erdemburak/dto/CreateCustomerRequest.java
+++ src/main/java/com/erdemburak/dto/CreateCustomerRequest.java
@@ -1,14 +1,12 @@
 package com.erdemburak.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class CreateCustomerRequest extends BaseCustomerRequest{
     private String id;
 }
