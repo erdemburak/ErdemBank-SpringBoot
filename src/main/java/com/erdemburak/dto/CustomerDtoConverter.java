759cd02be15d8d511e8c6f40a67bee9c09c1b5b4
erdemburak <burakerdem6@hotmail.com>
2024-02-07 13:35:20 +0300

    CustomerService - getAllCustomers methodu oluşturuldu

---
+++ src/main/java/com/erdemburak/dto/CustomerDtoConverter.java
@@ -0,0 +1,18 @@
+package com.erdemburak.dto;
+
+import com.erdemburak.model.Customer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerDtoConverter {
+    public CustomerDto convert(Customer customer){
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setId(customer.getId());
+        customerDto.setAddress(customer.getAddress());
+        customerDto.setName(customer.getName());
+        customerDto.setDateOfBirth(customer.getDateOfBirth());
+        customerDto.setCity(CityDto.valueOf(customer.getCity().name()));
+
+        return customerDto;
+    }
+}
--- src/main/java/com/erdemburak/service/CustomerService.java
+++ src/main/java/com/erdemburak/service/CustomerService.java
@@ -3,39 +3,48 @@ package com.erdemburak.service;
 import com.erdemburak.dto.CityDto;
 import com.erdemburak.dto.CreateCustomerRequest;
 import com.erdemburak.dto.CustomerDto;
+import com.erdemburak.dto.CustomerDtoConverter;
 import com.erdemburak.model.City;
 import com.erdemburak.model.Customer;
 import com.erdemburak.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerDtoConverter customerDtoConverter;
 
-    public CustomerService(CustomerRepository customerRepository) {
+    public CustomerService(CustomerRepository customerRepository, CustomerDtoConverter customerDtoConverter) {
         this.customerRepository = customerRepository;
+        this.customerDtoConverter = customerDtoConverter;
     }
 
 
     public CustomerDto createCustomer(CreateCustomerRequest customerRequest){
         Customer customer = new Customer();
         customer.setId(customerRequest.getId());
-        customer.setAddress(customer.getAddress());
-        customer.setName(customer.getName());
-        customer.setDateOfBirth(customer.getDateOfBirth());
+        customer.setAddress(customerRequest.getAddress());
+        customer.setName(customerRequest.getName());
+        customer.setDateOfBirth(customerRequest.getDateOfBirth());
         customer.setCity(City.valueOf(customerRequest.getCity().name()));
 
         customerRepository.save(customer);
 
-        CustomerDto customerDto = new CustomerDto();
-        customerDto.setId(customerRequest.getId());
-        customerDto.setAddress(customer.getAddress());
-        customerDto.setName(customer.getName());
-        customerDto.setDateOfBirth(customer.getDateOfBirth());
-        customerDto.setCity(CityDto.valueOf(customerRequest.getCity().name()));
-
-        return customerDto;
+        return customerDtoConverter.convert(customer);
     }
 
+    public List<CustomerDto> getAllCustomers() {
+        List<Customer> customerList = customerRepository.findAll();
+
+        List<CustomerDto> customerDtoList = new ArrayList<>();
+
+        for(Customer customer: customerList){
+            customerDtoList.add(customerDtoConverter.convert(customer));
+        }
+        return customerDtoList;
+    }
 }
