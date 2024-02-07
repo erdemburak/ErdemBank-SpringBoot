9b0e4b8751e1f4c48ebac5f97232af4adaf3c5d2
erdemburak <burakerdem6@hotmail.com>
2024-02-07 19:46:21 +0300

    @Builder eklendi

--- src/main/java/com/erdemburak/model/Customer.java
+++ src/main/java/com/erdemburak/model/Customer.java
@@ -2,16 +2,14 @@ package com.erdemburak.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Customer {
 
     @Id
